ea5f323724013a97c43ec948af2a5f10762e1a81
makpowerful <75123335+makpowerful@users.noreply.github.com>
2020-12-09 21:14:22 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-pytest -s  -v --html=./Reports/report.html  testCases/test_Login.py --browser chrome
-PAUSE
+pytest -s  -v --html=./Reports/report.html  testCases/test_RegressionSuite.py --browser chrome
+rem pytest -s  -v --html=./Reports/report.html  testCases/test_Login.py --browser chrome
+rem PAUSE
